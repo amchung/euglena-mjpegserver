7f5e1c634632006b77defd4c390d90956bd0988f
Alice-MacAir <amchung@stanford.edu>
2013-11-06 14:12:00 -0800

    debug paper

--- game-collect-object.js
+++ game-collect-object.js
@@ -16,12 +16,19 @@ function setupVidCanvas() {
     
 	window.onload = function() {
 		paper.setup('myCanvas');
-		var path = new Path.Rectangle([75, 75], [100, 100]);
-		path.strokeColor = 'black';
+		// Create a simple drawing tool:
+		var tool = new Tool();
+		var path;
 
-		view.onFrame = function(event) {
-			// On each frame, rotate the path by 3 degrees:
-			path.rotate(3);
+		// Define a mousedown and mousedrag handler
+		tool.onMouseDown = function(event) {
+			path = new Path();
+			path.strokeColor = 'black';
+			path.add(event.point);
+		}
+
+		tool.onMouseDrag = function(event) {
+			path.add(event.point);
 		}
 	}
 	
