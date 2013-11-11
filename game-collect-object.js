f1befeeb83cfa95c558edf70dba22de28d173048
Alice-MacAir <amchung@stanford.edu>
2013-11-11 11:26:38 -0800

    debug paper animation

--- game-collect-object.js
+++ game-collect-object.js
@@ -29,11 +29,11 @@ function getPNG(){
 			source: img.src,
 			position: origin
 		});
-		raster.onLoad = function()
-		{	
+		//raster.onLoad = function()
+		//{	
         	//compareFrame(img);
         	window.requestAnimFrame(getPNG);
-    	}
+    	//}
 	}
     img.src = "http://171.65.102.132:8080/?action=snapshot?t=" + new Date().getTime();
 }
