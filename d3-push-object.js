dae90ec02d9750d828460507ce81863fc5da8829
Alice-MacAir <amchung@stanford.edu>
2013-11-18 12:14:04 -0800

    debug drawObject loop

--- d3-push-object.js
+++ d3-push-object.js
@@ -153,7 +153,7 @@ function setupD3() {
 
 function resetObjects(){
 	for (var i = -1; ++i < n_max;) {
-		var x = 40 + Math.random() * (w-40), y = 40 + Math.random() * (h-40);
+		var x = 40 + Math.random() * (vid_width-40), y = 40 + Math.random() * (vid_height-40);
 		objects[i].path[0][0] = x;
         objects[i].path[0][1] = y;
 	}
