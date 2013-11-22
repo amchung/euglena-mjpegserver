073e76bc8056409598aff6d3ca8a1c2b7e33fd07
Alice-MacAir <amchung@stanford.edu>
2013-11-22 13:58:29 -0800

    led display added

---
+++ d3-joystick-2.js
@@ -0,0 +1,401 @@
+﻿window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+        window.setTimeout(callback, 1000 / 30);
+    };
+})();
+
+var control_canvas,
+c,        // c is the fg_canvas' context 2D
+halfWidth,
+halfHeight,
+leftPointerID = -1,
+leftPointerPos = new Vector2(0, 0),
+leftPointerStartPos = new Vector2(0, 0),
+leftVector = new Vector2(0, 0);
+arrow = new VectorLED(0, 0, 0, 0);
+
+var touches; // collections of pointers
+
+/////////////////////////////// ARDUINO SETUP 
+var IOBoard = BO.IOBoard;
+var IOBoardEvent = BO.IOBoardEvent;
+var Pin = BO.Pin;
+
+//// Variables
+// LED related
+var led1; //-90
+var led2; //0
+var led3; //90
+var led4; //180=-180
+var LEDloopON = false;
+var LEDcont = 1;
+var max_val;
+
+var frame = 0;
+var lastUpdateTime = 0;
+var acDelta = 0;
+var msPerFrame = 10;
+
+// Set to true to print debug messages to console
+BO.enableDebugging = true; 
+
+var host = window.location.hostname;
+// if the file is opened locally, set the host to "localhost"
+if (window.location.protocol.indexOf("file:") === 0) {
+        host = "171.65.102.132";
+    }
+var arduino = new IOBoard(host, 8887);
+///////////////////////////// ARDUINO SETUP END
+
+var username = "noname";
+var socket;
+
+document.addEventListener("DOMContentLoaded", init);
+        
+window.onorientationchange = resetCanvas;
+window.onresize = resetCanvas;
+
+function init() {
+    setupCanvas();
+    setupD3();
+    setupMotionDetection();
+    
+    touches = new Collection();
+    
+    arduino.addEventListener(IOBoardEvent.READY, onReady);
+}
+
+function onReady(event) {
+        // Remove the event listener because it is no longer needed
+        arduino.removeEventListener(IOBoardEvent.READY, onReady);
+
+        // Set Pin 5,6,9,10 to PWM
+        arduino.setDigitalPinMode(5, Pin.PWM);
+        arduino.setDigitalPinMode(6, Pin.PWM);
+        arduino.setDigitalPinMode(9, Pin.PWM);
+        arduino.setDigitalPinMode(10, Pin.PWM);
+
+        // Create an LED object to interface with the LED wired
+        // to the I/O board
+        led1 = arduino.getDigitalPin(5);
+        led2 = arduino.getDigitalPin(6);
+        led3 = arduino.getDigitalPin(9);
+        led4 = arduino.getDigitalPin(10);
+            
+        // jQuery part for the sliders
+    $("#n_slider").slider({
+        step: 1,
+        min : 1,
+        max : 20,
+        value: 4
+    });
+
+    $('#n_value').text("Value: 4");
+    
+    $( "#n_slider" ).bind( "slide", function(event, ui) {
+        var value = ui.value;
+        $('#n_value').text("Value: " + value);
+        n = value;
+    });    
+    
+    
+    $("#size_slider").slider({
+        step: 10,
+        min : 20,
+        max : 200,
+        value : 80
+    });
+
+    $('#size_value').text("Value: 80");
+    
+    $( "#size_slider" ).bind( "slide", function(event, ui) {
+        var value = ui.value;
+        $('#size_value').text("Value: " + value);
+        console.log(value);
+        l = value;
+    });
+       
+    $("#v_slider").slider({
+        step: 0.05,
+        min : 0.0,
+        max : 1.0,
+        value : 0.4
+    });
+
+    $('#v_value').text("Value: 0.40");
+    
+    $( "#v_slider" ).bind( "slide", function(event, ui) {
+        var value = ui.value;
+        $('#v_value').text("Value: " + value);
+        console.log(value);
+        v = value;
+    });
+     
+    $("#br_slider").slider({
+        step: 0.05,
+        min : 0.0,
+        max : 1.0,
+        value: 0
+    });
+
+    $('#br_value').text("Value: 0.00");
+    
+    $( "#br_slider" ).bind( "slide", function(event, ui) {
+        var value = ui.value;
+        $('#br_value').text("Value: " + value.toFixed(2));
+        brown_const = value;
+    });
+    
+
+        $('input[name=setUsername]').click(function(){
+        	if($('input[name=usernameTxt]').val() != ""){
+                username = $('input[name=usernameTxt]').val();
+                    var msg = {type:'setUsername', user:username};
+                    socket.json.send(msg);
+                }
+            $('#username').slideUp("slow",function(){
+                $('#entergame').slideDown("slow");
+            });
+        });
+        
+        $("input[name=gamestartBtn]").click(function(){
+            resetGame();
+        });
+        
+        socket = new io.connect('http://171.65.102.132:8088');
+        var chat = $('#chat');
+        var board = $('#board');
+
+        socket.on('connect', function() {
+                console.log("Connected");
+                //chat.html("<b>Connected!</b>");
+                socket.emit('message', {channel:'realtime'});
+        });
+        
+        socket.on('message', function(message){
+                chat.append(message + '<br />');
+        });
+                
+        socket.on('postscore', function(score){
+                board.empty();
+                for (var i=0;i<score.length;i++){
+                        if(i==0){
+                                board.append('<span style="color: #FA6600">'+ score[i][0]+'  :  '+score[i][1]+'</span> <br />');
+                        }else{
+                                board.append(score[i][0]+'  :  '+score[i][1]+ '<br />');
+                        }
+                }
+                board.fadeOut('fast');
+                board.fadeIn('fast');
+                board.fadeOut('fast');
+                board.fadeIn('fast');
+        });
+
+        socket.on('disconnect', function() {
+                console.log('disconnected');
+                chat.html("<b>Disconnect!</b>");
+        });
+
+        $("input[name=sendBtn]").click(function(){
+                var msg = {type:'chat',message:username + " : " + $("input[name=chatTxt]").val()}
+                socket.json.send(msg);
+                $("input[name=chatTxt]").val("");
+        });
+        
+    control_canvas.addEventListener('pointerdown', onPointerDown, false);
+    control_canvas.addEventListener('pointermove', onPointerMove, false);
+    control_canvas.addEventListener('pointerup', onPointerUp, false);
+    control_canvas.addEventListener('pointerout', onPointerUp, false);
+    
+    requestAnimFrame(joystick_draw);
+}
+
+function joystick_draw() {
+    c.clearRect(0, 0, control_canvas.width, control_canvas.height);
+        
+    c.beginPath();
+    c.moveTo(halfWidth, halfHeight-4);
+    c.strokeStyle = "rgba(250, 102, 0, 1)";
+    c.lineWidth = 2;
+    c.lineTo(halfWidth, halfHeight+4);
+    c.stroke();
+            
+    c.beginPath();
+    c.moveTo(halfWidth+4, halfHeight);
+    c.strokeStyle = "rgba(250, 102, 0, 1)";
+    c.lineWidth = 2;
+    c.lineTo(halfWidth-4, halfHeight);
+    c.stroke();
+    
+    drawCircles(halfWidth, halfHeight);
+    
+        //// mouse event loop
+    touches.forEach(function (touch) {
+        if (touch.identifier == leftPointerID) {
+            var alpha = arrow.trimArrow(leftVector, max_val);
+            
+            c.beginPath();
+            c.fillStyle = "rgba(250, 102, 0, 1)";
+            c.arc(leftPointerPos.x, leftPointerPos.y, 16, 0, Math.PI * 2, true);
+            c.fill();
+            
+            c.beginPath();
+            c.moveTo(leftPointerStartPos.x,leftPointerStartPos.y);
+            c.strokeStyle = "rgba(250, 102, 0, 1)";
+                        c.lineTo(leftPointerStartPos.x+max_val*(arrow.int2-arrow.int4),leftPointerStartPos.y+max_val*(arrow.int1-arrow.int3));
+                        c.lineWidth = 3;
+                        c.stroke();
+                        
+                        c.beginPath();
+                        c.fillStyle = "rgba(255, 255, 255, "+alpha*(LEDcont)+")";
+                    c.arc(halfWidth, halfHeight, 16, 0, Math.PI * 2, true);
+                    c.fill();
+                        
+                        c.beginPath();
+                c.fillStyle = "#fff"; 
+                c.fillText(alpha,halfWidth-10, halfHeight-25);
+                
+                c.beginPath();
+                c.fillStyle = "#dd6600";
+                var theta = leftVector.angle();
+                c.fillText(theta.toFixed(0),leftPointerPos.x+10, leftPointerPos.y-20);
+        }
+    });
+
+    requestAnimFrame(joystick_draw);
+    
+    //// LED control loop
+    var delta = Date.now() - lastUpdateTime;
+    if (acDelta > msPerFrame)
+    {
+        acDelta = 0;
+        if(LEDloopON) 
+        {
+                //LEDon = frame%2;
+                LEDcont = 1;
+                changeLED(LEDcont);
+        }
+        frame++;
+        if (frame >= 2) {frame = 0;}
+    } else
+    {
+        acDelta += delta;
+    }
+    lastUpdateTime = Date.now();
+}
+
+function drawCircles(xCenter,yCenter)
+{
+        var needle = new Vector2(max_val, 0);
+        for(var i=0;i<24*3;i++)
+        {
+                c.beginPath();
+                c.fillStyle = "rgba(255, 255, 255, 0.5)";
+                c.lineWidth = 3;
+                c.arc(xCenter + needle.x, yCenter + needle.y, 1, 0, Math.PI * 2, true);
+                c.fill();
+                needle.rotate(5,0);
+        }
+}
+
+function givePointerType(event) {
+    switch (event.pointerType) {
+        case event.POINTER_TYPE_MOUSE:
+            return "MOUSE";
+            break;
+        case event.POINTER_TYPE_PEN:
+            return "PEN";
+            break;
+        case event.POINTER_TYPE_TOUCH:
+            return "TOUCH";
+            break;
+    }
+}
+
+function onPointerDown(e) {
+    var newPointer = { identifier: e.pointerId, x: e.clientX, y: e.clientY, type: givePointerType(e) };
+    leftPointerID = e.pointerId;
+    //leftPointerStartPos.reset(e.clientX, e.clientY);
+    leftPointerStartPos.reset(halfWidth, halfHeight);
+    leftPointerPos.copyFrom(leftPointerStartPos);
+    leftVector.reset(0, 0);
+    arrow.reset(0, 0, 0, 0);
+    touches.add(e.pointerId, newPointer);
+}
+
+function onPointerMove(e) {
+    if (leftPointerID == e.pointerId) {
+        //leftPointerPos.reset(e.clientX, e.clientY);
+        leftPointerPos.reset(e.offsetX, e.offsetY);
+        leftVector.copyFrom(leftPointerPos);
+        //leftVector.minusEq(leftPointerStartPos);
+        leftVector.minusEq(leftPointerStartPos);
+        arrow.setArrow(leftVector, max_val);
+        LEDloopON = true;
+    }
+    else {
+        if (touches.item(e.pointerId)) {
+            touches.item(e.pointerId).x = e.clientX;
+            touches.item(e.pointerId).y = e.clientY;
+        }
+    }
+}
+
+function onPointerUp(e) {
+    if (leftPointerID == e.pointerId) {
+        leftPointerID = -1;
+        leftVector.reset(0, 0);
+    }
+    leftVector.reset(0, 0);
+    arrow.reset(0, 0, 0, 0);
+    touches.remove(e.pointerId);
+    LEDloopON = false;
+    changeLED(0);
+}
+
+function setupCanvas() {
+    control_canvas = document.getElementById('controlCanvas');
+    c = control_canvas.getContext('2d');
+    resetCanvas();
+    c.strokeStyle = "#ffffff";
+    c.lineWidth = 2;
+}
+
+// Arduino control functions
+function changeLED(LEDon) {
+    if(LEDon)
+    {
+            led1.value = arrow.int1;
+            led2.value = arrow.int2;
+            led3.value = arrow.int3;
+            led4.value = arrow.int4;
+    }
+    else
+    {
+            led1.value = 0;
+            led2.value = 0;
+            led3.value = 0;
+            led4.value = 0;
+        }
+}
+
+
+function resetCanvas(e) {
+    max_val = (document.getElementById("controlArea").offsetWidth-100)/2;
+    
+    // resize the canvas - but remember - this clears the canvas too.
+    
+    control_canvas.width = document.getElementById("controlArea").offsetWidth-20;
+    control_canvas.height = control_canvas.width-20;
+
+    halfWidth = (control_canvas.width)/2;
+    halfHeight = (control_canvas.height)/2;
+
+    // make sure we scroll to the top left. 
+    window.scrollTo(0, 0);
+}
---
+++ d3-object-2.js
@@ -0,0 +1,358 @@
+var obj_canvas,
+obj_c,
+cp_canvas = null;
+
+var canvas
+var video_canvas,
+vid_c;
+
+var brown_const=0;
+
+var vid_width = 640;
+var vid_height = 480;
+
+var canvas;
+var svg_led;
+var context;
+
+var vid_width = 640;
+var vid_height = 480;
+
+var l = 80,
+	n = 4,
+	v = 1/4;
+	
+var n_max = 20;
+
+function setupD3() {
+    canvas = d3.select("#canvasArea").append("canvas")
+        .attr("width", vid_width)
+        .attr("height", vid_height);
+    
+    svg_led = d3.select("#ledArea").append("svg")
+        .attr("width", 300)
+        .attr("height", 300);
+        
+    var shape_stage = svg_led.append("rect")
+    							.attr("x", 112.5)
+    							.attr("y", 112.5)
+    							.attr("width", 75)
+    							.attr("height", 75);
+    							.attr("fill", "black");
+            
+    context = canvas.node().getContext("2d");  
+
+	var w = 640,
+    	h = 480,
+    	m = 20,
+    	radius = l/2+10,
+    	degrees = 180 / Math.PI;
+    
+	var objects = d3.range(n_max).map(function() {
+  		var x = 40 + Math.random() * (w-40), y = 40 + Math.random() * (h-40);
+  		return {
+    		vx: 0,
+    		vy: 0,
+    		path: d3.range(m).map(function() { return [x, y]; }),
+    		count: 0,
+    		active: 0,
+    		size: l,
+    		color: "#FA6600"
+  		};
+	});
+
+	var svg = d3.select("#canvasArea").append("svg:svg")
+    	.attr("width", vid_width)
+    	.attr("height", vid_height);
+    		
+	var g = svg.selectAll("g")
+    	.data(objects)
+		.enter().append("svg:g");
+
+	var box = g.append("svg:rect");
+	
+	function drawObjects(){
+		for (var i = -1; ++i < n;) {
+    		var object = objects[i],
+        		x = object.path[0][0],
+        		y = object.path[0][1];
+
+    		// stuck at the walls.
+    		if (x < 0)
+    		{
+    			object.path[0][0] = 0;
+    		}
+    		if(x > w-l) 
+    		{
+    			object.path[0][0] = w-l;
+    		}
+    		if (y < 0)
+    		{
+    			object.path[0][1] = 0;
+    		}
+    		if (y > h-l)
+    		{
+    			object.path[0][1] = h-l;
+    		}	
+    		object.size = l;
+    		object.active = (i<n)?1:0;
+    	}
+    	box.attr("opacity", function(d,i){
+    		return d.active;
+    	});
+		box.attr("width", function(d,i){
+			return d.size;
+		});
+		box.attr("height", function(d,i){
+			return d.size;
+		});
+		box.attr("stroke", function(d,i){
+			return d.color;
+		});
+  		box.attr("transform", function(d) {
+    		return "translate(" + d.path[0] + ")";
+  		});
+  	}
+
+
+	d3.timer(function() {
+  		drawObjects();
+	});
+
+	window.setInterval(getVideo, 1000/20);
+		
+	function getVideo(){
+        getVidFrame("http://171.65.102.132:8080/?action=snapshot?t=" + new Date().getTime(), function(image) {
+            context.clearRect(0, 0, vid_width, vid_height);
+            context.drawImage(image, 0, 0, vid_width, vid_height);
+        });
+        
+    	function getVidFrame(path, callback) {
+            var image = new Image;
+            image.onload = function() {
+                callback(image);
+                compareFrame(image);
+            };
+            image.src = path;
+        }
+    }
+    
+    
+    function compareFrame(img1) {
+		// just compare if there are two pictures
+  		if ( img2 != null ) {
+    		var res=[0,0,0,0];
+    		try {
+    			for (var i = -1; ++i < n;){
+    				res = compare(img1, img2, objects[i].path[0][0], objects[i].path[0][1], 14, l/2+10); 
+    			    	if ((res[0]>400)||(res[1]>400)||(res[2]>400)||(res[3]>400)){
+            				res[0]=0;res[1]=0;res[2]=0;res[3]=0;
+    					}
+    				objects[i].path[0][0]+=(res[0]+res[2]-res[1]-res[3])*v+(Math.random()-0.5)*20*brown_const
+    				objects[i].path[0][1]+=(res[0]+res[1]-res[2]-res[3])*v+(Math.random()-0.5)*20*brown_const
+					objects[i].color = ((res[0]+res[1]+res[2]+res[3])>0)?"#FDAC0D":"#FA6600";
+    			}
+    		}
+    	catch(e) {
+    			// errors
+    		}
+		}
+		img2 = img1;
+	}
+
+}
+
+
+
+function game(){
+	switch(gamephase)
+	{
+		case 'pre-game':
+			// draw intro
+		break;
+		case 'game-rest':
+    		vid_c.fillStyle = "#fff"; 
+    		vid_c.font="14px sans-serif";
+    		vid_c.fillText('New target ...',200,20);
+    		vid_c.fillText('player : ' + username + '  score : ' +score_val ,20,20);
+		break;
+		case 'game-action':
+    		vid_c.fillStyle = "#fff"; 
+    		vid_c.font="14px sans-serif";
+    		vid_c.fillText('Run! Run! Run!',200,20);
+    		vid_c.fillText('player : ' + username + '  score : ' +score_val ,20,20);
+		break;
+		case 'gameover':		
+    		vid_c.fillStyle = "#fff"; 
+    		vid_c.font="14px sans-serif";
+    		vid_c.textAlign="start"; 
+    		vid_c.fillText('player : ' + username + '  score : ' +score_val ,20,20);
+    		vid_c.fillStyle = "rgba(253,172,13,1)"; 
+    		vid_c.font="40pt sans-serif";
+    		vid_c.textAlign="center"; 
+    		vid_c.fillText('GAME OVER',vid_width/2,vid_height/2-100);
+    		vid_c.fillStyle = "#fff"; 
+    		vid_c.font="20pt sans-serif";
+    		vid_c.fillText(username+' scored '+score_val,vid_width/2,vid_height/2);
+		break;
+		default:
+			vid_c.fillStyle = "#fff";
+			vid_c.font="14px sans-serif";
+    		vid_c.fillText('player : ' + username + '  score : ' +score_val ,20,20);
+	}
+}
+
+var shipX=vid_width/2,
+	shipY=vid_height/2,
+	shipRad,
+	shipL=20,
+	starX=40,
+	starY=20;
+var gamelevel;
+var int_star=-1;
+var int_engine=-1;
+var engine = false;
+var rest = false;
+var starTimer;
+var enginerTimer;
+var gamephase;
+var score_val = 0;
+var actionTimer;
+var unit=30;
+
+function setGameAction(){
+	actionTimer = window.requestAnimationFrame(actionLoop);
+}
+
+function setbackPreGame(){
+    score_val = 0;
+    hit = new Array();
+    gamelevel=1;
+    
+	shipX = vid_width/2;
+    shipY = vid_height/2;
+    
+    gamephase='pre-game';
+}
+
+function setGameOver(){
+  	window.cancelAnimationFrame(actionTimer);
+    actionTimer = undefined;
+}
+
+
+function actionLoop(){
+	switch(gamephase)
+	{
+		case 'rest':
+  			if(rest==false)
+			{
+				rest=true;
+				int_star=gamelevel+6;
+				getStarLocation(); // get new starX, starY, shipRad
+				starTimer=window.setInterval(showStar,500);
+			}
+			drawStar(shipX,shipY,starX,starY,gamelevel-(int_star-6));
+			drawShip(shipX,shipY,shipRad);
+			actionTimer = window.requestAnimationFrame(gameLoop);
+  		break;
+		case 'engine':
+			console.log(hit);
+  			if(hit>0)
+			{
+				window.clearInterval(engineTimer);
+				engine=false;
+				rest=false;
+				int_star=-1;
+				int_engine=-1;
+				gamephase='gameover';
+				gamelevel=-1;
+			}
+			else
+			{
+				score_val=score_val+10;
+				if(engine==false)
+				{
+					engine=true;
+					int_engine=gamelevel+4;
+					engineTimer=window.setInterval(runEngine,500);
+				}
+			}
+			drawStar(shipX,shipY,starX,starY,gamelevel-(int_star-6));
+			drawShip(shipX,shipY,shipRad);
+  		break;
+	}
+}
+
+
+function gameOver(){
+    var msg = {type:'sendscore', user:username, score:score_val};
+    socket.json.send(msg);
+}
+
+
+/*******************************************************************************
+  Copyright (C) 2009 Tom Stoeveken
+  This program is free software;
+  you can redistribute it and/or modify it under the terms of the
+  GNU General Public License, version 2.
+  See the file COPYING for details.
+*******************************************************************************/
+
+var img1 = null;
+var img2 = null;
+var md_canvas = null;
+
+function setupMotionDetection() {
+  md_canvas = document.getElementById('mdCanvas');
+  test_canvas = document.getElementById('testCanvas');
+  md_canvas.width = vid_width;
+  md_canvas.height = vid_height;
+}
+
+/*
+  compare two images and count the differences
+
+  input.: image1 and image2 are Image() objects
+  input.: threshold specifies by how much the color value of a pixel
+          must differ before they are regarded to be different
+
+  return: number of different pixels
+*/
+
+function compare(image1, image2, ptX, ptY, threshold, ObjR) {
+  var movement = new Array(0,0,0,0);
+  var md_ctx = md_canvas.getContext("2d");
+  var width = md_canvas.width/2, height = md_canvas.height/2;
+
+  // copy images into canvas element
+  // these steps scale the images and decodes the image data
+  md_ctx.drawImage(image1, 0, 0, width, height);
+  md_ctx.drawImage(image2, width, 0, width, height);
+
+  // this makes r,g,b,alpha data of images available
+  var pixels1 = md_ctx.getImageData(0, 0, width, height);
+  var pixels2 = md_ctx.getImageData(width, 0, width, height);
+  
+  // substract picture1 from picture2
+  // if they differ set color value to max,
+  // if the difference is below threshold set difference to 0.
+  for (var x = Math.round((ptX-ObjR)/2); x < Math.round((ptX+ObjR)/2); x++) {
+    for (var y = Math.round((ptY-ObjR)/2); y < Math.round((ptY+ObjR)/2); y++) {
+
+      // each pixel has a red, green, blue and alpha value
+      // all values are stored in a linear array
+      var i = x*4 + y*4*pixels1.width;
+
+      var ch0 = ((pixels1.data[i] - pixels2.data[i])>threshold)?255:0;
+      var ch1 = ((pixels1.data[i] - pixels2.data[i])>threshold)?255:0;
+      var ch2 = ((pixels1.data[i] - pixels2.data[i])>threshold)?255:0;
+
+      // count differing pixels
+      var n = (x<Math.round(ptX/2))?0:1;
+      var m = (y<Math.round(ptY/2))?0:2;
+      movement[n+m] += Math.min(1, ch0 + ch1 + ch2);
+    }
+  }
+  return movement;
+}
